85d4d5ccd0ba531809bd6037f15787c9d06c27fe
Sooraj Parameswaran <srjpn123@gmail.com>
2022-03-31 15:04:34 +0530

    initial commit

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NSPTech
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
